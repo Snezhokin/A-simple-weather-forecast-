f1c013dbafb5f14eb54f93c47b43fe104e91d21c
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-02-19 22:58:50 +0300

    add:function and created a dictionary headers with our User-Agent

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import requests
 from bs4 import BeatifulSoup
+
+def weather_cheсk(city):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 YaBrowser/25.12.0.0 Safari/537.36'
+
+    }
