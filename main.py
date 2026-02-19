c90cc836ff9c2c9722485aaef76d170094b66ef3
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-02-19 23:27:27 +0300

    add: We get an HTML page using bs4 and extract the information into the console

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def weather_cheсk(city):
         f"https://www.google.com/search?q=погода+в+{city}", 
         headers=headers)
     print(response)
+
+    soup = BeatifulSoup(res.text, 'html.parser')
+
+    temperature = soup.select("#wob_tm")[0].getText()
+    title = soup.select("#wob_dc")[0].getText()
+    humidity = soup.select("#wob_hm")[0].getText()
+    time = soup.select("#wob_dts")[0].getText()
+    wind = soup.select("#wob_ws")[0].getText()
