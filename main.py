26b7fc3fa58c43460c32f6e870131e1e960f3717
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-02-19 23:11:19 +0300

    Add: Sends a get requests and checks the statuss code

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ def weather_cheсk(city):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 YaBrowser/25.12.0.0 Safari/537.36'
 
     }
+
+    response= requestes.get(
+        f"https://www.google.com/search?q=погода+в+{city}", 
+        headers=headers)
+    print(response)
